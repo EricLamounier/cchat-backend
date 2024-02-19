eb4025f6d1bab42b4fc548d78b68d8f092e1aa7b
EricLamounier <eric_lamounier@outlook.com>
2024-02-19 13:28:27 -0300

    .

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const { WebSocketServer } = require("ws");
 const dotenv = require("dotenv");
-
 dotenv.config();
 
 const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
@@ -10,6 +9,7 @@ wss.on("connection", (ws) => {
 
     ws.on("message", (message) => {
         wss.clients.forEach(client => client.send(message.toString()));
+        console.log(message)
     });
 
     console.log('connected .');
