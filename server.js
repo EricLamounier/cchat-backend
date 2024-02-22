c69724d2a5a2b6a4d0dcc9ca050015eb4c202e48
EricLamounier <eric_lamounier@outlook.com>
2024-02-22 11:48:27 -0300

    .

--- server.js
+++ server.js
@@ -1,57 +1,9 @@
-/*const { WebSocketServer } = require("ws");
-const dotenv = require("dotenv");
-dotenv.config();
-
-const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
-
-wss.on("connection", (ws, req) => {
-    const origin = req.headers.origin;
-	console.log(origin)
-
-    // Verificar se a origem é permitida
-    if (origin !== "http://localhost:3000") {
-        console.log('Conexão rejeitada. Origem não permitida:', origin);
-        ws.terminate(); // Terminar a conexão não autorizada
-        return;
-    }
-
-    ws.on("pong", () => {
-        ws.isAlive = true;
-    });
-
-    ws.on("error", console.error);
-
-    ws.on("message", (message) => {
-        const msg = message.toString();
-        wss.clients.forEach((client) => client.send(msg));
-        console.log(msg);
-    });
-
-    console.log('Conexão permitida de:', origin);
-
-    // Configurar intervalo de ping-pong
-    const pingInterval = setInterval(() => {
-        if (ws.isAlive === false) {
-            console.log('Terminando conexão inativa de:', origin);
-            return ws.terminate();
-        }
-
-        ws.isAlive = false;
-        ws.ping(null, undefined);
-    }, 30000); // 30 segundos
-
-    // Limpar intervalo quando a conexão é fechada
-    ws.on('close', () => {
-        clearInterval(pingInterval);
-    });
-});
-*/
 const { WebSocketServer } = require("ws");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
-
+const onlineUsers = []
 wss.on("connection", (ws) => {
     ws.isAlive = true;
 
@@ -62,12 +14,16 @@ wss.on("connection", (ws) => {
     ws.on("error", console.error);
 
     ws.on("message", (message) => {
-        const msg = message.toString();
-        wss.clients.forEach((client) => client.send(msg));
-        console.log(msg);
+        const msgSTRING = message.toString();
+        const msgJSON = msgSTRING.JSON()
+        wss.clients.forEach((client) => client.send(msgSTRING));
+        console.log(msgJSON);
+
+        if(msgJSON.messageType == -1) // Login
+            onlineUsers.push(msgJSON)
     });
 
-    console.log('connected.');
+    console.log(onlineUsers)
 
     // Configurar intervalo de ping-pong
     const pingInterval = setInterval(() => {
