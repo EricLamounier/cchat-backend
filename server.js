b7cffdbc6c6f72d618d66e57dc00b3429f701b7e
EricLamounier <eric_lamounier@outlook.com>
2024-02-23 00:06:17 -0300

    .

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ dotenv.config();
 
 const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
 const onlineUsers = {};
-let clientID = 0
+let clientID = 0;
+
 wss.on("connection", (ws) => {
+    ws.isAlive = true;
 
     ws.on("pong", () => {
         ws.isAlive = true;
@@ -14,46 +16,54 @@ wss.on("connection", (ws) => {
 
     ws.on("error", console.error);
 
-    ws.on("message", (message) => {
-        let content = message.toString();
-        const msgJSON = JSON.parse(content);
-        clientID = msgJSON.userID || 0
-        
-        if (msgJSON.messageType == 0){ // Login
-            onlineUsers[msgJSON.userID] = ws;
-            const users = {
-                'messageType': 1, // Usuarios
-                'users': Object.keys(onlineUsers),
-            }
-            content = JSON.stringify(users)
-        }
-
-        wss.clients.forEach((client) => client.send(content));
-    });
+    ws.on("message", handleMessage);
 
     // Configurar intervalo de ping-pong
     const pingInterval = setInterval(() => {
-        console.log(Object.keys(onlineUsers));
         if (ws.isAlive === false) {
-            console.log(`Terminating inactive connection for clientId ${clientID}`);
-            clearInterval(pingInterval);
-            delete onlineUsers[clientID];
-            return ws.terminate();
+            terminateConnection(ws, "Terminating inactive connection");
+            return;
         }
 
         ws.isAlive = false;
         ws.ping(null, undefined);
-    }, 5000); // 5 segundos
+    }, 120000); // 120 segundos
 
     // Limpar intervalo quando a conexão é fechada
     ws.on('close', () => {
-        console.log(`Terminating connection for clientID ${clientID}`);
-        delete onlineUsers[clientID];
-        const users = {
-            'messageType': 1, // Usuarios
-            'users': Object.keys(onlineUsers),
-        }
-        wss.clients.forEach((client) => client.send(JSON.stringify(users)));
-        clearInterval(pingInterval);
+        terminateConnection(ws, "Terminating connection");
     });
 });
+
+function handleMessage(message) {
+    let content = message.toString();
+    const msgJSON = JSON.parse(content);
+    clientID = msgJSON.userID || 0;
+
+    if (msgJSON.messageType === 0) { // Login
+        onlineUsers[msgJSON.userID] = this;
+        broadcastUsers();
+    } else if (msgJSON.messageType === 3) { // Logout
+        terminateConnection(this, "Terminating connection due to logout");
+    } else {
+        wss.clients.forEach((client) => client.send(content));
+    }
+}
+
+function terminateConnection(ws, logMessage) {
+    console.log(`${logMessage} for clientID ${clientID}`);
+    clearInterval(ws.pingInterval);
+    delete onlineUsers[clientID];
+    broadcastUsers();
+    ws.terminate();
+}
+
+function broadcastUsers() {
+    const users = {
+        'messageType': 1, // Usuarios
+        'users': Object.keys(onlineUsers),
+    };
+    wss.clients.forEach((client) => {
+        client.send(JSON.stringify(users));
+    });
+}
